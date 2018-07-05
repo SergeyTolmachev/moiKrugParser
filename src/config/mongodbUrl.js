b30a2d27247819c8e4ef4f277bacca104cd72ced
SergeyTolmachev <stolmachov@gmail.com>
2018-07-05 13:41:09 +0300

    change directories

---
+++ src/config/mongodbUrl.js
@@ -0,0 +1,5 @@
+mongodbUrl = 'mongodb://dbuser:develop1992@ds261929.mlab.com:61929/firstdb';
+
+module.exports.mongodbUrl = mongodbUrl;
+
+//строка подключения к базе данных может иметь разные формы и должна содержать в себе логин и пароль, поэтому обратитесь к документации mongoDb
