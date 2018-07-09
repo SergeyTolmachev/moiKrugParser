58b9e30235fe9cb0ec4ea5bb47a5818327f118c4
SergeyTolmachev <stolmachov@gmail.com>
2018-07-09 05:32:17 +0300

    with right working logger

--- src/config/mongodbUrl.js
+++
@@ -1,5 +0,0 @@
-mongodbUrl = 'mongodb://dbuser:develop1992@ds261929.mlab.com:61929/firstdb';
-
-module.exports.mongodbUrl = mongodbUrl;
-
-// строка подключения к базе данных может иметь разные формы и должна содержать в себе логин и пароль, поэтому обратитесь к документации mongoDb
